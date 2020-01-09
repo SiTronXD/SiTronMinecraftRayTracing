95288dad8f84061d41a36321ac5300ac176985a4
Bensson <47039664+SiTronXD@users.noreply.github.com>
2020-01-09 08:29:44 +0100

    Minecraft test texture added

--- MinecraftRayTracingProj/core/src/game/MainGame.java
+++ MinecraftRayTracingProj/core/src/game/MainGame.java
@@ -1,7 +1,8 @@
-package com.sitron.game;
+package game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -13,16 +14,21 @@ public class MainGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("gfx/minecraftTextureSheet.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 0, -400);
 		batch.end();
+		
+		// DEBUGGING!!!!!!!
+		if(Gdx.input.isKeyJustPressed(Keys.ESCAPE))
+			Gdx.app.exit();
 	}
 	
 	@Override
--- MinecraftRayTracingProj/desktop/src/com/sitron/game/desktop/DesktopLauncher.java
+++ MinecraftRayTracingProj/desktop/src/com/sitron/game/desktop/DesktopLauncher.java
@@ -2,7 +2,8 @@ package com.sitron.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.sitron.game.MainGame;
+
+import game.MainGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
