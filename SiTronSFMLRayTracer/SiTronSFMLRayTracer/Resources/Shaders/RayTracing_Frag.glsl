a372cf045a21700aae77e9bf641a7dcf085e044f
Bensson <47039664+SiTronXD@users.noreply.github.com>
2020-10-09 10:47:44 +0200

    Added ordered dithering to screen-door transparency

--- SiTronSFMLRayTracer/SiTronSFMLRayTracer/Resources/Shaders/RayTracing_Frag.glsl
+++ SiTronSFMLRayTracer/SiTronSFMLRayTracer/Resources/Shaders/RayTracing_Frag.glsl
@@ -3,7 +3,7 @@
 // Mode 0: Stochastic transparency using blue noise
 // Mode 1: Stochastic transparency using animated blue noise
 // Mode 2: Screen-door transparency
-#define TRANSPARENCY_MODE 0
+#define TRANSPARENCY_MODE 2
 
 // Enable/disable god rays
 #define GOD_RAYS_ENABLED 1
@@ -21,15 +21,25 @@ const int NUM_FOG_SAMPLES = 8;
 const float FOG_DENSITY = 0.5;
 const float GOLDEN_RATIO_CONJUGATE = 0.618033f; // also just fract(goldenRatio)
 
+const float screenDoorGridSize = 80.0;
+const float bayerMatrix8x8[8*8] = float[]
+(
+	0.0  / 64.0,	48.0 / 64.0,	12.0 / 64.0,	60.0 / 64.0,	3.0  / 64.0,	51.0 / 64.0,	15.0 / 64.0,	63.0 / 64.0,
+	32.0 / 64.0,	16.0 / 64.0,	44.0 / 64.0,	28.0 / 64.0,	35.0 / 64.0,	19.0 / 64.0,	47.0 / 64.0,	31.0 / 64.0,
+	8.0	 / 64.0,	56.0 / 64.0,	4.0  / 64.0,	52.0 / 64.0,	11.0 / 64.0,	59.0 / 64.0,	7.0  / 64.0,	55.0 / 64.0,
+	40.0 / 64.0,	24.0 / 64.0,	36.0 / 64.0,	20.0 / 64.0,	43.0 / 64.0,	27.0 / 64.0,	39.0 / 64.0,	23.0 / 64.0,
+	2.0  / 64.0,	50.0 / 64.0,	14.0 / 64.0,	62.0 / 64.0,	1.0  / 64.0,	49.0 / 64.0,	13.0 / 64.0,	61.0 / 64.0,
+	34.0 / 64.0,	18.0 / 64.0,	46.0 / 64.0,	30.0 / 64.0,	33.0 / 64.0,	17.0 / 64.0,	45.0 / 64.0,	29.0 / 64.0,
+   	10.0 / 64.0,	58.0 / 64.0,	6.0  / 64.0,	54.0 / 64.0,	9.0  / 64.0,	57.0 / 64.0,	5.0  / 64.0,	53.0 / 64.0,
+	42.0 / 64.0,	26.0 / 64.0,	38.0 / 64.0,	22.0 / 64.0,	41.0 / 64.0,	25.0 / 64.0,	37.0 / 64.0,	21.0 / 64.0
+);
+
 // Uniforms
 uniform int u_numValidBlocks;
 
 const float MAX_RAY_DISTANCE = 64.0;
 
 uniform float u_time;
-//uniform float u_blockIndex[NUM_MAX_BLOCKS];
-//uniform float u_blockSpecular[NUM_MAX_BLOCKS];
-//uniform float u_blockTransparency[NUM_MAX_BLOCKS];
 
 uniform vec2 u_resolution;
 
@@ -144,7 +154,7 @@ bool isTransparent(float alpha, vec2 normalizedUV)
 	// Screen-door
 	#elif (TRANSPARENCY_MODE == 2)
 
-		const int pixelSizeFactor = 10;
+		/*const int pixelSizeFactor = 10;
 		const float gridSizeFactor = 40.0;
 
 		float s = pixelSizeFactor * 2.0 - 1.0;
@@ -152,7 +162,15 @@ bool isTransparent(float alpha, vec2 normalizedUV)
 		float c1 = abs(round((fract(st.x)-0.5) * (s+1.0)) / s);
 		float c2 = abs(round((fract(st.y)-0.5) * (s+1.0)) / s);
     
-		transparencyThreshold = clamp(c1 + c2, 0.05f, 0.95f);
+		transparencyThreshold = clamp(c1 + c2, 0.05f, 0.95f);*/
+
+
+		vec2 st = floor(normalizedUV * screenDoorGridSize);
+
+		transparencyThreshold = bayerMatrix8x8[
+			(int(st.x) % 8) + 
+			(int(st.y) % 8) * 8
+		] + 0.5 / 64.0;
 
 	#endif
 
